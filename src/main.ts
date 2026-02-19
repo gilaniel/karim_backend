14e92953b31d4aaa77a8301767c4a95d3eff6810
gila <gilaniel@yandex.ru>
2026-02-19 11:20:09 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    app.enableCors({
-    origin: ['http://localhost:5173', 'https://karim-nu.vercel.app'],
+  app.enableCors({
+    origin: ["http://localhost:5173", "https://karim-nu.vercel.app"],
     credentials: true,
   });
 
